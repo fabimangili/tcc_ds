c6bf783f45abb1baf87f088f72964a5605df13c1
fabimangili <106878152+fabimangili@users.noreply.github.com>
2022-06-04 12:11:05 -0300

    Add files via upload
    Extração dos dados do CPC a partir dos pares de coordenadas

---
+++ extracao_cpc.py
@@ -0,0 +1,17 @@
+import pandas as pd
+import xarray as xr
+import csv
+
+def extract (variavel):
+    with open('coord_cpc.csv') as csvDataFile:
+        csvReader = csv.reader(csvDataFile)
+        for row in csvReader:
+            ds = xr.open_mfdataset(variavel+'/*.nc')
+            dsloc = ds.sel(lon=float(row[1]), lat=float(row[2]), method='nearest')
+            var, time = dsloc[variavel].values, dsloc['time'].values
+            tempo = pd.DataFrame(var, time)
+            exp_csv = tempo.to_csv('extraido_50/' + variavel + str(row[0]) + '.csv', header=False)
+
+#extract('tmax') #tmin #tmax #precip
+
+
